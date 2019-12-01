c0df20879e873a2dfc60a7145547e67d5d94344b
BoscoTin <tintin111198@gmail.com>
2019-12-02 01:57:39 +0800

    Chatroom model

--- src/Component/Pages/Main/index.jsx
+++ src/Component/Pages/Main/index.jsx
@@ -1,11 +1,32 @@
 import React from 'react'
 
+import Chatroom from '../../Prefabs/chatroom.jsx'
+
+import {
+  Container,
+  Row,
+  Col
+} from 'react-bootstrap'
+
+import "../../styles.css"
+
+
+const messages = []
+
 
 class MainPage extends React.Component{
   render(){
     return(
-      <div>
-        Main page
+      <div className="mainpage">
+        <Row>
+          <Col>
+            The chatroom is at the right.
+          </Col>
+
+          <Col>
+            <Chatroom messages={messages} />
+          </Col>
+        </Row>
       </div>
     )
   }
---
+++ src/Component/Prefabs/chatroom.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "../styles.css"
+
+
+const Chatroom = ({messages}) => (
+  <div className="chatroom">
+
+  </div>
+)
+
+export default Chatroom
