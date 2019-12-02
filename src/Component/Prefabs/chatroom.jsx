d557b7f4bff830c7e1344deef6e66b00075bb4bc
BoscoTin <tintin111198@gmail.com>
2019-12-02 20:50:51 +0800

    Input bar interface

--- src/Component/Prefabs/chatroom.jsx
+++ src/Component/Prefabs/chatroom.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
 import "../styles.css"
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { ChatActions } from '../../actions'
+
+import { InputRow } from "./input.jsx"
+
+
 
 const Chatroom = ({messages}) => (
   <div className="chatroom">
-
+    <div className="messages_box">
+      { /* messages.map( message => <div>{message}</div>) */}
+    </div>
+    <InputRow />
   </div>
 )
 
-export default Chatroom
+
+function mapStateToProps(state) {
+  return {
+    messages: [
+      "abc",
+      "edf"
+    ]
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  const map = bindActionCreators({...ChatActions}, dispatch);
+  map.dispatch = dispatch;
+  return map;
+}
+
+export default connect(
+  mapStateToProps, mapDispatchToProps
+)(Chatroom)
