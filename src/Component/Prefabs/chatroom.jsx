c162c9fd41d3003115dab41af9e68746da9a7f6d
BoscoTin <tintin111198@gmail.com>
2019-12-02 21:37:51 +0800

    Update webpack config

--- src/Component/Prefabs/chatroom.jsx
+++ src/Component/Prefabs/chatroom.jsx
@@ -6,24 +6,150 @@ import { bindActionCreators } from 'redux';
 import { ChatActions } from '../../actions'
 
 import { InputRow } from "./input.jsx"
+import { Message } from "./message.jsx"
 
 
+class Chatroom extends React.Component{
 
-const Chatroom = ({messages}) => (
-  <div className="chatroom">
-    <div className="messages_box">
-      { /* messages.map( message => <div>{message}</div>) */}
-    </div>
-    <InputRow />
-  </div>
-)
+  scrollToEnd = () => {
+    this.messagesEnd.scrollIntoView({ behavior: "smooth" });
+  }
+
+  componentDidMount(){
+    this.scrollToEnd()
+  }
+
+  componentDidUpdate(){
+    this.scrollToEnd()
+  }
+
+  render(){
+    const messages = this.props.messages
+
+    return (
+      <div className="chatroom">
+          <ul className="messages_box">
+            {messages.map( message => (
+              <Message message={message} />
+            ))}
+            <div style={{ float:"left", clear: "both" }}
+              ref={(el) => { this.messagesEnd = el; }}>
+            </div>
+          </ul>
+          <InputRow />
+        </div>
+    )
+  }
+}
 
 
 function mapStateToProps(state) {
   return {
     messages: [
-      "abc",
-      "edf"
+      {
+        content: "Hi",
+        user: true
+      },
+      {
+        content: "Bye",
+        user: false
+      },
+      {
+        content: "Hi",
+        user: true
+      },
+      {
+        content: "Bye",
+        user: false
+      },
+      {
+        content: "Hi",
+        user: true
+      },
+      {
+        content: "Bye",
+        user: false
+      },
+      {
+        content: "Hi",
+        user: true
+      },
+      {
+        content: "Bye",
+        user: false
+      },
+      {
+        content: "Hi",
+        user: true
+      },
+      {
+        content: "Bye",
+        user: false
+      },
+      {
+        content: "Hi",
+        user: true
+      },
+      {
+        content: "Bye",
+        user: false
+      },
+      {
+        content: "Hi",
+        user: true
+      },
+      {
+        content: "Bye",
+        user: false
+      },
+      {
+        content: "Hi",
+        user: true
+      },
+      {
+        content: "Bye",
+        user: false
+      },
+      {
+        content: "Hi",
+        user: true
+      },
+      {
+        content: "Bye",
+        user: false
+      },
+      {
+        content: "Hi",
+        user: true
+      },
+      {
+        content: "Bye",
+        user: false
+      },
+      {
+        content: "Hi",
+        user: true
+      },
+      {
+        content: "Bye",
+        user: false
+      },
+      {
+        content: "Hi",
+        user: true
+      },
+      {
+        content: "Bye",
+        user: false
+      },
+      {
+        content: "Hi",
+        user: true
+      },
+      {
+        content: "Bye",
+        user: false
+      }
     ]
   };
 }
--- src/Component/Prefabs/message.jsx
+++ src/Component/Prefabs/message.jsx
@@ -1,1 +1,12 @@
 import React from 'react'
+import "../styles.css"
+
+
+
+const Message = ({message}) => (
+  <li className={`chat ${message.user ? "right" : "left"}`}>
+    {message.content}
+  </li>
+)
+
+export { Message }
